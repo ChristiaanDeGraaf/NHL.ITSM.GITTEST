6b64343686910f6dd9e2af4b383f938c67f12f6c
Christiaan de Graaf <Christiaan.de.Graaf@hotmail.com>
2023-10-22 18:47:13 +0200

    test 2

--- NHL.ITSM.GITTEST/Program.cs
+++ NHL.ITSM.GITTEST/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Lets do this again");
+Console.WriteLine("Lets do this again and again");
